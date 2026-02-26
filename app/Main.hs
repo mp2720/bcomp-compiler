9616c518c2a486092709d8d829e393a868ed2e8c
Salomakhin Timur <mp2720@proton.me>
2026-02-26 09:09:03 +0100

    Main.hs: add initial cli
    Signed-off-by: Salomakhin Timur <mp2720@proton.me>

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,52 @@
 module Main where
 
-import AST (dump)
+import AST (StringDump (dump))
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Except (ExceptT (runExceptT), except, throwE)
+import Data.Bifunctor (first)
+import Data.List (intercalate)
+import IR.FromAST (convert)
 import Parse.Combinators (Parser (runParser), eof)
-import Parse.Grammar
+import Parse.Grammar (program)
 import Parse.Lexer (lexerPosition, regularLexerState)
+import System.Directory (createDirectoryIfMissing)
+import System.Environment (getArgs)
+import System.Exit (ExitCode (ExitFailure), exitWith)
+import System.IO (hPutStrLn, stderr)
+import Text.Printf (printf)
 
-splitByChar :: String -> [String]
-splitByChar [] = []
-splitByChar s = foldr f ([[] | last s == '\n']) s
-  where
-    f '\n' toks = [] : toks
-    f c [] = [[c]]
-    f c toks@(_ : _) = (c : head toks) : tail toks
+compile :: ExceptT String IO ()
+compile = do
+  args <- liftIO getArgs
+  source <- case args of
+    [sourcePath] -> liftIO $ readFile sourcePath
+    _ -> throwE "invalid usage"
+
+  (ast, _) <-
+    except $
+      first
+        (printf "syntax error at %s" . show . lexerPosition)
+        (runParser (program <* eof) (regularLexerState source))
+
+  liftIO $ createDirectoryIfMissing True "output"
+
+  -- TODO: make output path configurable
+
+  liftIO $ writeFile "output/prog.ast" (dump ast)
+
+  prog <-
+    except $
+      first
+        (intercalate "\n" . map show)
+        (convert ast)
+
+  liftIO $ writeFile "output/prog.ir" (show prog)
 
 main :: IO ()
 main = do
-  let s = "auto v1;auto v2=1+*2;"
-  case runParser (program <* eof) (regularLexerState s) of
-    Left state -> print (lexerPosition state)
-    Right (ast, _) -> putStrLn $ dump ast
+  result <- runExceptT compile
+  case result of
+    Left err -> do
+      hPutStrLn stderr err
+      exitWith (ExitFailure 1)
+    Right () -> pure ()
